93be8a95507ef1560dcb3e38c99da21ce1720a06
Jeevan Kandagatla <jeevankandakatla@gmail.com>
2025-01-26 16:27:13 +0530

    Updated settings.py

--- venkicars/venkicars/settings.py
+++ venkicars/venkicars/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'venkicars.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'venkicars.wsgi.application'
 
 
 # Database
